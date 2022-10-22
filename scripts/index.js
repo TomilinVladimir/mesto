592c79d4ec0a05af98d9f43ebed1f1cd4c5c8b52
Vladimir Tomilin <Xsenustomilin@yandex.ru>
2022-10-22 21:08:06 +0200

    6.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const cardSelectors = {
   form: '.popup__input-card',
   addCard: '.profile__add-button',
@@ -36,9 +9,7 @@ const cardSelectors = {
   inputSrc: '.popup__input_card_src',
   buttonDel: '.element__del',
   likeButton: '.like-button',
-  // imgBtn: '.element__img',
 }
-
 
 const profilBtnEdit = document.querySelector('.profile__edit-button');
 const popupProfil = document.querySelector('.popup_profil');
@@ -67,12 +38,8 @@ const imgText = document.querySelector('.popup__img-text');
 
 const newCardName = cardForm.querySelector(cardSelectors.inputName);
 const newCardSrc = cardForm.querySelector(cardSelectors.inputSrc);
-
-// const popupProfilOwerlay = popupProfil.querySelector('.popup__overlay');
-// const popupCardOwerlay = popupCard.querySelector('.popup__overlay');
-// const popupZoomOwerlay = popupZoom.querySelector('.popup__overlay');
-
-
+const popupList = document.querySelectorAll('.popup');
+const cardButtonAdd = cardForm.querySelector('.popup__input-save');
 
 // ESC закрытие
 const ESC_KEYCODE = 27;
@@ -95,34 +62,7 @@ function openPopup(form) {
   document.addEventListener('keydown', handleEscUp);
 }
 
-function closePopup(form) {
-  form.classList.remove('popup_visible');
-}
-
-// function openProfilForm() {
-//   popupProfil.classList.add('popup_visible');
-//   nameInput.value = oldName.textContent;
-//   jobInput.value = oldJob.textContent;
-// }
-
-// function closeProfilForm() {
-//   popupProfil.classList.remove('popup_visible');
-// }
-
-// function openCardForm() {
-//   popupCard.classList.add('popup_visible');
-// }
-
-// function closeCardForm() {
-//   popupCard.classList.remove('popup_visible');
-// }
-
-// function closeZoomForm() {
-//   popupZoom.classList.remove('popup_visible');
-// }
-
-
-function profilSubmitHandler(evt) {
+function submitHandlerProfil(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
@@ -134,29 +74,26 @@ function profilSubmitHandler(evt) {
   closePopup(popupProfil);
 }
 
-function addCardFunc(name, src) {
+function createCardFunc(name, src) {
   const newElement = template.cloneNode(true);
   const cardName = newElement.querySelector(cardSelectors.text);
   const cardImg = newElement.querySelector(cardSelectors.img);
   const btnDel = newElement.querySelector(cardSelectors.buttonDel);
   const likeButton = newElement.querySelector(cardSelectors.likeButton);
-  // const imgBtn = newElement.querySelector(cardSelectors.imgBtn);
 
   cardName.textContent = name;
   cardImg.alt = name;
   cardImg.src = src;
-  // cardsContainer.prepend(newElement);
 
   likeButton.addEventListener('click', function (like) {
     like.target.classList.toggle('like-button_active');
   });
 
   cardImg.addEventListener('click', function () {
-    openPopup(popupZoom);
-    // popupZoom.classList.add('popup_visible');
     urlImg.setAttribute('src', cardImg.src);
     urlImg.setAttribute('alt', name);
     imgText.textContent = name;
+    openPopup(popupZoom);
   });
 
   btnDel.addEventListener('click', function () {
@@ -164,18 +101,16 @@ function addCardFunc(name, src) {
   });
 
   return newElement;
-
 }
 
 function renderCards(name, src) {
-  const todo = addCardFunc(name, src);
+  const todo = createCardFunc(name, src);
   cardsContainer.prepend(todo);
 }
 
-function cardSubmitListner() {
+function submitCardListner() {
   cardForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    // addCardFunc(newCardName.value, newCardSrc.value);
     renderCards(newCardName.value, newCardSrc.value);
     closePopup(popupCard);
     cardForm.reset();
@@ -186,11 +121,9 @@ function createInitialCards() {
   initialCards.forEach(function (item) {
     const cardName = item.name;
     const cardSrc = item.link;
-    // addCardFunc(cardName, cardSrc);
     renderCards(cardName, cardSrc);
   })
 }
-
 
 function openProfilPopup() {
   openPopup(popupProfil);
@@ -198,25 +131,24 @@ function openProfilPopup() {
   jobInput.value = oldJob.textContent;
 }
 
+function openCardForm(btn) {
+  openPopup(popupCard);
+  btn.classList.add('popup__button_disabled');
+  btn.setAttribute('disabled', true);
+}
+
 // // // Прикрепляем обработчик к форме:
 // // // он будет следить за событием “submit” - «отправка»
-profilForm.addEventListener('submit', profilSubmitHandler);
+profilForm.addEventListener('submit', submitHandlerProfil);
 
-profilBtnEdit.addEventListener('click', function () { openPopup(popupProfil) });
+profilBtnEdit.addEventListener('click', function () { openProfilPopup() });
 profilBtnClose.addEventListener('click', function () { closePopup(popupProfil) });
 
-
-cardBtnAdd.addEventListener('click', function () { openPopup(popupCard) });
+cardBtnAdd.addEventListener('click', function () { openCardForm(cardButtonAdd) });
 cardBtnClose.addEventListener('click', function () { closePopup(popupCard) });
 
 zoomBtnClose.addEventListener('click', function () { closePopup(popupZoom) });
 
-
-// popupProfilOwerlay.addEventListener('click', closeProfilForm);
-// popupCardOwerlay.addEventListener('click', closeCardForm);
-// popupZoomOwerlay.addEventListener('click', closeZoomForm);
-
-const popupList = document.querySelectorAll('.popup');
 popupList.forEach(popup => {
   popup.addEventListener('mousedown', (evt) => {
     if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
@@ -225,6 +157,5 @@ popupList.forEach(popup => {
   })
 });
 
-
-cardSubmitListner();
+submitCardListner();
 createInitialCards();
